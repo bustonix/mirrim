b2edad0775bc3c4b0a05f8eebd85f2068f0a23eb
Gaye <bustonix@gmail.com>
2026-01-10 04:51:24 +0100

    chore: disable google indexing (noindex, nofollow)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,10 @@ export const metadata: Metadata = {
     type: "website",
     locale: "fr_FR",
   },
+  robots: {
+    index: false,
+    follow: false,
+  },
 };
 
 export default function RootLayout({
